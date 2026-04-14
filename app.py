f96603b4302a328566fe9bd052d34190dc8b4c24
Nikki <113893391+HonestGiftless@users.noreply.github.com>
2026-04-14 12:07:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -100,6 +100,6 @@ def handle_answer(data):
         }, room='game_room')
 
 # запуск сервера
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 5000))
+#    socketio.run(app, host='0.0.0.0', port=port)
